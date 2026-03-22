7c4161bf2ada00cb1a58450ac22d3fced6b2c349
Ruwanya Abeynayake <ruwanya.abeynayake@thakshana.com>
2026-03-22 07:58:03 +0530

    Added the msp header file

---
+++ Demonstrations/Module_2/Video_3/msp.h
@@ -0,0 +1,66 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+
+ /**
+  * @file msp.h
+  * @brief This file contains the definitions and function prototypes for the MSPboard.
+  * 
+  * @author Ruwanya Abeynayake
+  * @date 2026-03-22
+  * 
+  */
+
+#ifndef __MSP_H__
+#define __MSP_H__
+
+/**
+ * @brief This struct defines the message types for platform
+ * 
+ * This struct defines the message types for platform which includes MSG_INIT, MSG_STATUS, 
+ * MSG_STOP
+ * 
+ * @param MSG_INIT: Message type for initialization
+ * @param MSG_STATUS: Message type for status update
+ * @param MSG_STOP: Message type for stopping the platform
+ * 
+ * @return None
+ */
+
+ typedef enum {
+    MSG_INIT,
+    MSG_STATUS,
+    MSG_STOP
+} message_type_t;
+
+/**
+ * @brief This function prints the platfrom using, MSP
+ * 
+ * This function prints the platfrom currently usinng, for MSP
+ * 
+ * @param None
+ * 
+ * @return None
+ */
+void msp_platform_print(void);
+
+/**
+ * @brief Logs the messages based on types
+ * 
+ * This function prints the messages based on the message type given as input,
+ * which the message types are defined in the message_type_t enum. 
+ * 
+ * @param type: The message type to be logged
+ * 
+ * @return None
+ */
+void msp_platform_log(message_type_t type);
+
+#endif /* __MSP_H__ */
